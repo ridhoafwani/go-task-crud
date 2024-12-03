542766cec38c104181eac6a9478a5ea23d02905e
ridhoafwani <zikrurridhoafwani@gmail.com>
2024-12-03 19:29:57 +0700

    feat : impement mutex for locking mechanism

--- internal/repositories/task/create_task.go
+++ internal/repositories/task/create_task.go
@@ -3,6 +3,9 @@ package repositories
 import models "task-crud/internal/models/task"
 
 func (repo *InMemoryTaskRepository) Create(task models.Task) (models.Task, error) {
+	repo.mutex.Lock()
+	defer repo.mutex.Unlock()
+
 	task.ID = repo.nextID
 	repo.tasks[repo.nextID] = task
 	repo.nextID++
--- internal/repositories/task/repository_task.go
+++ internal/repositories/task/repository_task.go
@@ -1,21 +1,25 @@
 package repositories
 
-import model "task-crud/internal/models/task"
+import (
+	"sync"
+	models "task-crud/internal/models/task"
+)
 
 type InMemoryTaskRepository struct {
-	tasks  map[int]model.Task
+	tasks  map[int]models.Task
 	nextID int
+	mutex  sync.RWMutex
 }
 
 func NewInMemoryTaskRepository() *InMemoryTaskRepository {
 	return &InMemoryTaskRepository{
-		tasks:  make(map[int]model.Task),
+		tasks:  make(map[int]models.Task),
 		nextID: 1,
 	}
 }
 
 type TaskRepository interface {
-	Create(task model.Task) (model.Task, error)
+	Create(task models.Task) (models.Task, error)
 	// GetAll() ([]model.Task, error)
 	// GetByID(id int) (model.Task, error)
 	// Update(id int, task model.Task) (model.Task, error)
